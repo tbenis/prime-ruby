28b69a4e310f4615398ff03108ed7ddfe9b682f5
Benis Tambe <will.jenifer@yahoo.com>
2021-01-16 23:11:12 -0500

    commit tree 110cc2e658bae18c03ee00b8ec3317708f74a6f0

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(int)
+    if int <=1
+        return false
+    elsif int == 2 || int == 3
+        return true
+    elsif int % 2 == 0 || int % 3 == 0
+        puts "i got here"
+        return false
+    end
+     i = 5
+    while i * i <= int 
+    if int % i === 0 || int % (i+2) === 0
+        return false
+    end 
+        i += 6
+    end
+    true
+end
